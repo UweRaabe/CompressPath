fff605b9df7ba5f482efc7f2f9b6a88d3c1f52bb
Uwe Raabe <uwe@uweraabe.de>
2020-05-29 16:45:46 +0200

    environment variables are expanded duplicates are removed command line help store shortcuts support for Delphi 10.4

--- source/CompressPath.dpr
+++ source/CompressPath.dpr
@@ -24,6 +24,7 @@ type
     FRegistry: TRegistry;
     FShortcuts: TStringList;
     FVariables: TStringList;
+    function ExpandEnvironmentVars(const Value: string): string;
   protected
     procedure AddShortCut(const Key, Value: string);
     procedure AddDelphiShortCut(const Key, RelExePath, RelDocPath: string); overload;
@@ -33,6 +34,7 @@ type
     function GetExistingPath(const Prefix: array of string; const RelPath: string): string; overload;
     procedure InitShortCuts;
     function ReadRegistry(const AName: string): string;
+    procedure RemoveDuplicates;
     function StorePath: Integer;
     procedure StoreVariables;
     procedure WriteRegistry(const Name, Value: string);
@@ -67,6 +69,9 @@ begin
   FPath.Delimiter := ';';
   FPath.StrictDelimiter := true;
   {$IFDEF DEBUG}
+  { It is quite unlikely that we have write permission to HKEY_LOCAL_MACHINE when we are running from inside the IDE.
+    In addition it doesn't make much sense to run in DEBUG mode outside of the IDE. Hence this restriction. There are
+    other places where we check for DEBUG mode. In case you get unexpected results, please have a look for those. }
   FRegistry := TRegistry.Create(KEY_READ);
   {$ELSE}
   FRegistry := TRegistry.Create();
@@ -96,6 +101,7 @@ end;
 
 procedure TPathCompressor.AddShortCut(const Key, Value: string);
 begin
+  { Shortcuts are only usefull when they contain some data. }
   if Value > '' then begin
     Shortcuts.Values[Key] := Value;
   end;
@@ -104,12 +110,17 @@ end;
 procedure TPathCompressor.AddDelphiShortCut(const Key, RelExePath, RelDocPath: string);
 begin
   AddShortCut(Key, GetExistingPath(PathDelphi, RelExePath));
+  { Depending on the type of installation (current user or all users), the BPLs are located
+    in different documents folders. Here we first check inside the personal folder and
+    if that doesn't exist, we take the public documents folder.
+  }
   AddShortCut(Key + 'BPL', GetExistingPath([PathMyDocs, PathSharedDocs], RelDocPath));
 end;
 
 procedure TPathCompressor.AddOldDelphiShortCut(const Key, RelExePath, RelDocPath: string);
 begin
   AddShortCut(Key, GetExistingPath(PathDelphi, RelExePath));
+  { In older Delphi versions the BPL folder resides below the Delphi folder. }
   AddShortCut(Key + 'BPL', GetExistingPath(PathDelphi, RelDocPath));
 end;
 
@@ -120,6 +131,9 @@ var
   L: Integer;
   I: Integer;
 begin
+  { Find the Shortcut that will replace the longest string from the beginning of Value.
+    Ideally it will replace the whole string.
+  }
   N := -1;
   L := 0;
   for I := 0 to Shortcuts.Count - 1 do begin
@@ -131,24 +145,42 @@ begin
   end;
 
   if L > 0 then begin
+    { Replace the Shortcut and remember to add a similar variable. }
     result := Value.Remove(0, L).Insert(0, '%' + Shortcuts.Names[N] + '%');
     Variables.Append(Shortcuts[N]);
   end
   else begin
+    { Nothing to compress. }
     result := Value;
   end;
 end;
 
 procedure TPathCompressor.Execute(out OldLength, NewLength: Integer);
+{ Returns the old and new length of the PATH variable. }
 var
   I: Integer;
 begin
+  Variables.Clear;
   OldLength := OrgLength;
   for I := 0 to Path.Count - 1 do begin
     Path[I] := Compress(Path[I]);
   end;
+  RemoveDuplicates;
   StoreVariables;
   NewLength := StorePath;
+end;
+
+function TPathCompressor.ExpandEnvironmentVars(const Value: string): string;
+var
+  buffer: TCharArray;
+  res: Cardinal;
+begin
+  Result := Value;
+  SetLength(Buffer, 32*1024);
+  res := ExpandEnvironmentStrings(PChar(Value), @Buffer[0], Length(Buffer) - 1);
+  if res > 0 then begin
+    Result := PChar(@Buffer[0]);
+  end;
 end;
 
 function TPathCompressor.GetExistingPath(const Prefix: array of string; const
@@ -170,7 +202,7 @@ end;
 
 procedure TPathCompressor.InitShortCuts;
 begin
-  { Allgemeine Programmpfade }
+  { Program Files }
   AddShortCut('PF', PathProgramFiles);
   AddShortCut('PF86', PathProgramFilesX86);
 
@@ -178,12 +210,12 @@ begin
   AddShortCut('SQL', GetExistingPath(PathProgramFiles, '\Microsoft SQL Server'));
   AddShortCut('SQL86', GetExistingPath(PathProgramFilesX86, '\Microsoft SQL Server'));
 
-  { TODO : Pfade für fehlende Delphi-Versionen ergänzen}
+  { TODO : Add folders for missing Delphi versions}
 
   { Delphi 7 }
   AddOldDelphiShortCut('D7', '\Borland\Delphi7', '\Borland\Delphi7\Projects\Bpl');
 
-  { Delphi ab D2007 }
+  { Delphi D2007+ }
   AddDelphiShortCut('D2007', '\CodeGear\RAD Studio\5.0', '\RAD Studio\5.0\Bpl');
   AddDelphiShortCut('D2009', '\CodeGear\RAD Studio\6.0', '\RAD Studio\6.0\Bpl');
   AddDelphiShortCut('D2010', '\Embarcadero\RAD Studio\7.0', '\RAD Studio\7.0\Bpl');
@@ -194,6 +226,12 @@ begin
   AddDelphiShortCut('XE5', '\Embarcadero\RAD Studio\12.0', '\RAD Studio\12.0\Bpl');
   AddDelphiShortCut('XE6', '\Embarcadero\Studio\14.0', '\Embarcadero\Studio\14.0\Bpl');
   AddDelphiShortCut('XE7', '\Embarcadero\Studio\15.0', '\Embarcadero\Studio\15.0\Bpl');
+  AddDelphiShortCut('XE8', '\Embarcadero\Studio\16.0', '\Embarcadero\Studio\16.0\Bpl');
+  AddDelphiShortCut('DX', '\Embarcadero\Studio\17.0', '\Embarcadero\Studio\17.0\Bpl');
+  AddDelphiShortCut('DX1', '\Embarcadero\Studio\18.0', '\Embarcadero\Studio\18.0\Bpl');
+  AddDelphiShortCut('DX2', '\Embarcadero\Studio\19.0', '\Embarcadero\Studio\19.0\Bpl');
+  AddDelphiShortCut('DX3', '\Embarcadero\Studio\20.0', '\Embarcadero\Studio\20.0\Bpl');
+  AddDelphiShortCut('DX4', '\Embarcadero\Studio\21.0', '\Embarcadero\Studio\21.0\Bpl');
 end;
 
 function TPathCompressor.LoadPath(UserEnvironment: Boolean): Boolean;
@@ -216,7 +254,7 @@ begin
   result := Registry.OpenKey(RegKey, false);
   if result then begin
     S := ReadRegistry('Path');
-    Path.DelimitedText := S;
+    Path.DelimitedText := ExpandEnvironmentVars(S);
     FOrgLength := S.Length;
   end;
 end;
@@ -246,6 +284,35 @@ begin
   result := Registry.ReadString(AName);
 end;
 
+procedure TPathCompressor.RemoveDuplicates;
+var
+  lst: TStringList;
+  S: string;
+  I: Integer;
+  N: Integer;
+begin
+  lst := TStringList.Create;
+  try
+    lst.Sorted := true;
+    lst.Duplicates := dupIgnore;
+    for S in Path do begin
+      lst.Add(S);
+    end;
+    I := 0;
+    while I < Path.Count do begin
+      if lst.Find(Path[I], N) then begin
+        lst.Delete(N);
+        Inc(I);
+      end
+      else begin
+        Path.Delete(I);
+      end;
+    end;
+  finally
+    lst.Free;
+  end;
+end;
+
 function TPathCompressor.StorePath: Integer;
 var
   S: string;
@@ -268,6 +335,8 @@ end;
 procedure TPathCompressor.WriteRegistry(const Name, Value: string);
 begin
   {$IFDEF DEBUG}
+  { In DEBUG mode only show the changes that would be made to the registry.
+  }
   Writeln(Name, '=', Value);
   Exit;
   {$ENDIF}
@@ -277,6 +346,22 @@ begin
     Registry.WriteString(Name, Value);
 end;
 
+procedure ShowHelp;
+var
+  exeName: string;
+begin
+  exeName := TPath.GetFileNameWithoutExtension(ParamStr(0));
+  Writeln('Compresses the system and user PATH variable using environment variables.');
+  Writeln;
+  Writeln(exeName, ' [/F importfile] [/X exportfile]');
+  Writeln;
+  Writeln('  /F importfile'.PadRight(20), 'load shortcuts from <importfile>');
+  Writeln('  /X exportfile'.PadRight(20), 'store shortcuts into <exportfile>');
+  Writeln;
+  Writeln('You must have Administrator rights to change the system PATH variable!');
+  Writeln;
+end;
+
 procedure Main;
 var
   instance: TPathCompressor;
@@ -284,10 +369,17 @@ var
   lOld: Integer;
   shortCutFileName: string;
 begin
+  if FindCmdLineSwitch('?') then begin
+    ShowHelp;
+    Exit;
+  end;
   instance := TPathCompressor.Create;
   try
     if FindCmdLineSwitch('f', shortCutFileName) then begin
       instance.LoadShortCuts(shortCutFileName);
+    end;
+    if FindCmdLineSwitch('x', shortCutFileName) then begin
+      instance.StoreShortCuts(shortCutFileName);
     end;
     Write('HKEY_LOCAL_MACHINE: ');
     if instance.LoadPath(false) then begin
@@ -321,6 +413,7 @@ begin
       Writeln(E.ClassName, ': ', E.Message);
   end;
   {$IFDEF DEBUG}
+  { When started from within the IDE we want to see the output. }
   Write('Press <Enter> to continue...');
   Readln;
   {$ENDIF}
